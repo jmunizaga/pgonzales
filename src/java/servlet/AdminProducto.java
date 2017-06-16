1e0a0f572fe54f802d191e9b2379204343e4858d
jmunizaga@bitbucket <jaime.munizaga.l@gmail.com>
2017-06-16 03:06:49 -0400

    first commit

--- src/java/servlet/AdminProducto.java
+++ src/java/servlet/AdminProducto.java
@@ -1,16 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package servlet;
 
 import beans.BeanProductosLocal;
 import clases.Producto;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
